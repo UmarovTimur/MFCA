dfd4a0d2cde9d4c14bfe21bbac8e8c6b56f2716f
Timur <asas92asas2@gmail.com>
2025-01-15 14:44:39 +0500

    add parser

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
+const MYSQL = require('mysql2/promise');
+
 // ydeROEcQGJUoe^8sh6vCLP5%
+const HOST = '195.35.59.22';
+const USER = 'u684340623_lib24';
+const PASSWORD = 'Xg?>/G+9s';
+const DATABASE = 'u684340623_lib24';
+const URL = "https://mfca.uzlatin.com//wp-json/wp/v2/posts";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsIm5hbWUiOiJ0aW0zNDUiLCJpYXQiOjE3MzY4NjM1NjQsImV4cCI6MTg5NDU0MzU2NH0.pyVCb35rod1OYkzRqeJtP9pSC6iYSD_xXy9B9rUkcYs";   // используйте, если настроен JWT
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsIm5hbWUiOiJ0aW0zNDUiLCJpYXQiOjE3MzY4NjM1NjQsImV4cCI6MTg5NDU0MzU2NH0.pyVCb35rod1OYkzRqeJtP9pSC6iYSD_xXy9B9rUkcYs
-// jwt 
-// const username = "your-username";
-// const password = "your-password"; // только для Basic Auth (не рекомендуется в продакшене)
+const TABLE_NAME = 'books_az';
 
-const apiUrl = "https://mfca.uzlatin.com//wp-json/wp/v2/posts";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsIm5hbWUiOiJ0aW0zNDUiLCJpYXQiOjE3MzY4NjM1NjQsImV4cCI6MTg5NDU0MzU2NH0.pyVCb35rod1OYkzRqeJtP9pSC6iYSD_xXy9B9rUkcYs";   // используйте, если настроен JWT
-const mysql = require('mysql2/promise');
 
 async function connectToDatabase() {
     try {
-        const connection = await mysql.createConnection({
-            host: '195.35.59.22',
-            user: 'u684340623_lib24',
-            password: 'Xg?>/G+9s',
-            database: 'u684340623_lib24'
+        const connection = await MYSQL.createConnection({
+            host: HOST,
+            user: USER,
+            password: PASSWORD,
+            database: DATABASE
         });
 
-        const [rows] = await connection.execute('SELECT * FROM books_az');
-        // console.log('Результаты:', rows);
+        const [rows] = await connection.execute(`SELECT * FROM ${TABLE_NAME} LIMIT 1`);
 
-        rows.forEach(row => {
-            if (row.id == 27) {
-                console.log(row.title);
-            }
-        });
+        const options = parseBook(rows[0]);
+        // createPost(options);
+
+        // rows.forEach(async (row) => {
+        //     const options = parseBook(row);
+        //     createPost(options);
+        // });
+
+        // setTimeout(() => {
+        //     removeFeaturedImage(rows[0].id);
+        // }, 500);
+
 
         await connection.end();
     } catch (err) {
@@ -34,20 +42,20 @@ async function connectToDatabase() {
 }
 
 // Функция для создания поста
-async function createPost(title, options) {
+async function createPost(options) {
+    console.log(options);
     try {
-        const response = await fetch(apiUrl, {
+        const response = await fetch(URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`, // Для JWT
-                // "Authorization": `Basic ${btoa(username + ":" + password)}`, // Для Basic Auth
+                "Authorization": `Bearer ${TOKEN}`, // Для JWT
             },
             body: JSON.stringify({
-                title: title,
                 ...options,
             }),
         });
+
 
         if (!response.ok) {
             throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -71,7 +79,87 @@ function getCategoreisId() {
             console.error("Failed to fetch categories:", error);
         });
 }
+// createPost(title,);
+function parseBook(book) {
+    const type = TABLE_NAME.split('_')[0];
+    const lang = TABLE_NAME.split('_')[1];
 
-// connectToDatabase();
-// createPost("Заголовок поста", );
+    return {
+        title: book.title,
+        status: "publish",
+        meta: {
+            fifu_image_url: `https://mukitob.com/${type}/covers/${lang}/${book.id}.jpg`, // Передаём URL изображения        
+        },
+        content: `
+        <p>${book?.author}</p>
+        <h3 class="wp-block-heading">${book.title}</h3>
+        <p>${book.annotation ? `${book.annotation}` : `${book?.title2}`}</p>
+    `,
+        categories: [18, 17],
+        featured_media: 447
+    };
+}
+
+
+async function getImages() {
+    try {
+        const response = await fetch("https://mfca.uzlatin.com//wp-json/wp/v2/media", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${TOKEN}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch images! Status: ${response.status}`);
+        }
+
+        const images = await response.json();
+        console.log("Images:", images); // Вернёт массив изображений с их ID, URL и другой информацией
+
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+
+async function removeFeaturedImage(postId) {
+    try {
+        const response = await fetch(`${URL}/${postId}`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${TOKEN}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                featured_media: 0 // Устанавливаем значение 0 для удаления изображения
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to update post! Status: ${response.status}`);
+        }
+
+        const postData = await response.json();
+        console.log("Post updated successfully:", postData);
+
+    } catch (error) {
+        console.error("Error removing featured image:", error);
+    }
+}
+
+
+
+
+
+connectToDatabase();
 // getCategoreisId();
+
+// Пример использования
+// getAllMedia(TOKEN);
+
+
+// Пример использования
+// getImages();
+
